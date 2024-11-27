cc7bddb0f96fdc4f3b6fe047a1f816753085a36e
Aliasgar <aliasgartafazzul000@gmail.com>
2024-11-27 18:44:56 +0530

    New changes in main.py file

---
+++ main1.py
@@ -0,0 +1,30 @@
+# This is a Escape Sequence Character (\n)
+#   print("Hey,\nfriend")
+#   print("Hey\"I am a boy\"")
+#   print("Hey", 3 , 4 , 5, sep="~", end="003\n")
+
+# Varialbles and Data type
+#   a = 1 
+#   b = 1.0
+#   c = "Ali"
+#   d = None
+#   print("The type of a is",type(a))
+#   print("The type of b is",type(b))
+#   print("The type of c is",type(c))
+#   print("The type of d is",type(d))
+
+#Type Casting (Explicit & Implicit)
+#   a = "1" 
+#   b = "2"
+#   print(int(a) + int(b))
+# Implicit 
+#   c = 1.9
+#   d = 54
+
+#   print(c + d)
+# Explicit
+#   string = "24"
+#   number = 3
+#   string_number = int(string)
+#   sum = number + string_number
+#   print("The sum of both the number is:",sum)
---
+++ main2.py
@@ -0,0 +1,26 @@
+#         Taking User Input
+
+#   a = input("Enter you name:",)
+#   print("My name is",a)
+#   x = input('Enter first No.') 
+#   y = input('Enter second No.')
+#   print(x + y)
+#   print(int(x) + int(y))
+
+#           Strings
+# apple = '''aldifnahkdmniknd
+# djkjkidjfmkl,slkjlad
+# amdvjaoimfkdj'''
+# name = "Aliasgar"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])
+# print(name[6])
+# print(name[7])  
+# print("Lets use a for loop\n")
+# for character in apple:
+#     print(character)
+
